edae22db84952ab119e12fd8451247b5dd912e3a
Tianhang Liu <tianhanl@usc.edu>
2018-02-11 22:16:39 -0800

    refine the handling of data

--- wiki.py
+++ wiki.py
@@ -1,5 +1,7 @@
 import wikipedia
+import re
 from bs4 import BeautifulSoup
+from functools import reduce
 
 wikipedia.set_lang('en')
 
@@ -9,16 +11,30 @@ def get_page_html(query):
     # Let wikipedia suggest one for us
     return wikipedia.page(wikipedia.search(query)[0]).html()
 
+def clean_text(text):
+    result = re.sub('\n+',' ',text)
+    result = re.sub(' +',' ', result)
+    result = bytes(result, 'UTF-8')
+    result = result.decode('ascii', 'ignore')
+    result = result.strip()
+    return result
+
 
 def parse_table(table):
     # get all the keys
     keys = list(map(lambda x: x.text.replace('\n', ''), table.find_all('th')))
+    result = []
 
+    flag = False;
+    for key in keys[0:3]:
+        if re.search('poll', key, re.IGNORECASE) is not None:
+            flag = True
     # If no keys contain poll, assume the table is not related with polling data
     # if not any('poll' in key for key in keys):
     #     return []
+    if not flag:
+        return result
 
-    result = []
     for row in table.find_all('tr'):
         cells = row.find_all('td')
 
@@ -29,7 +45,7 @@ def parse_table(table):
 
         curr_result = {}
         for index, cell in enumerate(cells):
-            curr_result[keys[index]] = cell.text.replace('\n', '')
+            curr_result[keys[index]] = clean_text(cell.text)
         if len(curr_result) != 0:
             result.append(curr_result)
     return result
@@ -40,5 +56,8 @@ def get_poll_tables(html_raw):
     bs_obj = BeautifulSoup(html_raw, "html.parser")
     tables = bs_obj.find_all('table', {'class': 'wikitable'})
     tables = list(map(lambda x: parse_table(x), tables))
-    tables = list(filter(lambda x:len(x ), tables))
+    tables = list(filter(lambda x:len(x)>0, tables))
+    tables = list(reduce((lambda x, y: x+y), tables))
     return tables
+
+print(get_poll_tables(get_page_html('south korea presidential election')))
