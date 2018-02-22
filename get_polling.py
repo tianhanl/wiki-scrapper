1d3dea84d2b51a83438c740b7b862fd37291a7ae
Tianhang Liu <tianhanl@usc.edu>
2018-02-21 22:03:41 -0800

    add function to command line tool to get tables from url

--- get_polling.py
+++ get_polling.py
@@ -5,10 +5,19 @@ import sys
 if __name__ == '__main__':
     from sys import argv
     import wiki
+    import re
     if len(argv) < 2:
         print('usage: python3 get_polling "query"')
     else:
         query = argv[1]
-        polling_tables = wiki.get_poll_tables(wiki.get_page_html(query))
+        url_pattern = 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
+        # check is query a link
+        if re.search(url_pattern, query, re.IGNORECASE) is not None:
+            # if query is a link, directly use the link to get page content
+            polling_tables = wiki.get_poll_tables(
+                wiki.get_page_html_from_url(query))
+        else:
+            polling_tables = wiki.get_poll_tables(wiki.get_page_html(query))
+
         wiki.save_tables(polling_tables,
                          query.lower().replace(' ', '_') + '.json')
